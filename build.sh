7f0b5e70a19a72f60fa226088e49d2fea83e3cef
vetermanve <github@vetermanve.com>
2022-02-23 17:57:29 +0300

    - excellent build script

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
-#!/bin/sh
+#!/bin/bash -x
+version='1.0'
+dockerProject="vetermanve/wishlist"
+commitCount=$(git rev-list --count HEAD);
+branch=$(git rev-parse --abbrev-ref HEAD);
 
-docker build -t vetermanve/wishlist:latest -f .docker/php-worker-production/Dockerfile .
+if [[ 'main' == $branch ]]; then
+  tags=" -t $dockerProject:$version.$commitCount -t $dockerProject:latest "
+else
+  tags=" -t $dockerProject:$version.$commitCount-$branch "
+fi
+
+docker build $tags -f .docker/php-worker-production/Dockerfile .
+docker push
